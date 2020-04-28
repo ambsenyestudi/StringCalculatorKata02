fb2c12bb24e81fe01356c4dc505440dc48864200
ambsenyestudi <adrifre7@hotmail.com>
2020-04-28 19:47:07 +0200

    Setup

---
+++ src/StringCalculator/StringCalculator.Test/string_calculcator_should.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace StringCalculator.Test
+{
+    public class string_calculcator_should
+    {
+        [Fact]
+        public void be_true()
+        {
+            Assert.True(true);
+        }
+    }
+}
