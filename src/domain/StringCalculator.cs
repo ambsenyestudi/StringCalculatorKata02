35b803d8dd85eeac8529b8186e4992824fb57244
ambsenyestudi <adrifre7@hotmail.com>
2022-09-02 07:48:01 +0200

    Extracts method

--- src/domain/StringCalculator.cs
+++ src/domain/StringCalculator.cs
@@ -5,13 +5,19 @@ public class StringCalculator
 
     public int Add(string numberInput)
     {
-        if (string.IsNullOrWhiteSpace(numberInput))
-        {
-            return 0;
-        }
+        
         if(numberInput.Contains(SEPARATOR))
         {
             return numberInput.Split(SEPARATOR).Select(x => int.Parse(x)).Sum();
+        }
+        return ParseInput(numberInput);
+    }
+
+    private int ParseInput(string numberInput)
+    {
+        if (string.IsNullOrWhiteSpace(numberInput))
+        {
+            return 0;
         }
         return int.Parse(numberInput);
     }
