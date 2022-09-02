a3ded648e04161469aff2ec63c8f564fa639c7db
ambsenyestudi <adrifre7@hotmail.com>
2022-09-02 19:46:40 +0200

    Implements separator definition

--- src/domain/StringCalculator.cs
+++ src/domain/StringCalculator.cs
@@ -3,12 +3,14 @@ public class StringCalculator
 {
     private const string SEPARATOR = ",";
     private const string NEW_LINE = "\n";
+    private const string CHARATER_DEFINITION_STARTER = "//";
 
     public int Add(string numberInput)
     {
-        if(numberInput == "//;\n1;2")
+        if(TryGetSepartor(numberInput, out string separator))
         {
-            return 3;
+            var inputMembers = RemoveSeparatorDefinition(numberInput).Split(separator);
+            return ParseInput(inputMembers).Sum();
         }
         if(numberInput.Contains(SEPARATOR) || numberInput.Contains(NEW_LINE))
         {
@@ -17,6 +19,20 @@ public class StringCalculator
         }
         return ParseInput(numberInput);
     }
+
+    private static bool TryGetSepartor(string numberInput, out string separator)
+    {
+        
+        if(!numberInput.StartsWith(CHARATER_DEFINITION_STARTER))
+        {
+            separator = string.Empty;
+            return false;
+        }
+        separator = numberInput.Split(NEW_LINE).First().Replace(CHARATER_DEFINITION_STARTER, string.Empty);
+        return true;
+    }
+    private static string RemoveSeparatorDefinition(string numberInput) =>
+        numberInput.Split(NEW_LINE).Last();
 
     private static IEnumerable<int> ParseInput(IEnumerable<string> numberInput) =>
         numberInput.Select(x => ParseInput(x));
