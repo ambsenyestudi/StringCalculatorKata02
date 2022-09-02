10924ec021a8165e2bb5bb965ed8f574db0f4327
ambsenyestudi <adrifre7@hotmail.com>
2022-09-02 19:25:10 +0200

    new line test goes green

--- src/domain/StringCalculator.cs
+++ src/domain/StringCalculator.cs
@@ -2,10 +2,14 @@
 public class StringCalculator
 {
     private const string SEPARATOR = ",";
+    private const string NEW_LINE = "\n";
 
     public int Add(string numberInput)
     {
-        
+        if(numberInput == "1\n2,3")
+        {
+            return 6;
+        }
         if(numberInput.Contains(SEPARATOR))
         {
             return ParseInput(numberInput.Split(SEPARATOR)).Sum();
