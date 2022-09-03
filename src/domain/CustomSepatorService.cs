e3a97fdb5ffe38b015943e5744cc1f79fb90aa0d
ambsenyestudi <adrifre7@hotmail.com>
2022-09-03 08:49:32 +0200

    Uses regex to identify definition

--- src/domain/CustomSepatorService.cs
+++ src/domain/CustomSepatorService.cs
@@ -1,10 +1,13 @@
-﻿namespace StringCalculation.Domain
+﻿using System.Text.RegularExpressions;
+
+namespace StringCalculation.Domain
 {
     public class CustomSepatorService
     {
-
+        private const string DEFINITION_PATTERN = "^[//].[\n]";
+        private readonly Regex _separatorRegex = new Regex(DEFINITION_PATTERN + "*");
         public bool StartsWithDefintion(string input) =>
-                input.StartsWith("//;\n");
+            _separatorRegex.IsMatch(input);
 
         public int Split(string numberInput)
         {
