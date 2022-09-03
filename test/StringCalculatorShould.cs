97874e7fc5bb11a79a2a4f7802e0481e68700f84
ambsenyestudi <adrifre7@hotmail.com>
2022-09-03 09:14:13 +0200

    Adds red reject negatives test

--- test/StringCalculatorShould.cs
+++ test/StringCalculatorShould.cs
@@ -22,4 +22,13 @@ public class StringCalculatorShould
         var result = _stringCalculator.Add(input);
         Assert.Equal(expected, result);
     }
+
+    [Fact]
+    public void Reject_Negatives()
+    {
+        var input = "1,-2,-3";
+        var expected = "error: negatives not allowed: -2 -3";
+        var exception = Assert.Throws<ArgumentException>(() => _stringCalculator.Add(input));
+        Assert.Equal(expected, exception.Message);
+    }
 }
